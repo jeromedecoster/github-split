70720be3601e2deaa6676ded37d0d351cc665d71
jeromedecoster <jeromedecoster@users.noreply.github.com>
2020-05-01 10:05:20 +0200

    :boom:

--- script.sh
+++ script.sh
@@ -1,24 +1,36 @@
 set -e
 
-log() { echo -e "\033[0;4m${1}\033[0m ${@:2}"; }
-
-# abort if sudo access is already enabled
-[[ -n $(sudo -n uptime 2>/dev/null) ]] && { log abort root access unauthorized; exit; }
-
-# ask sudo access
-log warn sudo access required...
-sudo echo >/dev/null
-# one more check if the user abort the password question
-[[ -z `sudo -n uptime 2>/dev/null` ]] && { log abort sudo required; exit; }
+log() { echo -e "\e[38;5;82;4m${1}\e[0m \e[38;5;226m${@:2}\e[0m"; }
+err() { echo -e "\e[38;5;196;4m${1}\e[0m \e[38;5;87m${@:2}\e[0m" >&2; }
 
 cd /usr/local/bin
 
-sudo curl raw.githubusercontent.com/jeromedecoster/github-split/master/github-split \
-    --location \
-    --remote-name \
-    --progress-bar
+# abort if already installed
+[[ -x github-split ]] && { log abort github-split already exists; exit 0; }
 
-sudo chmod ug+x github-split
-sudo chown $UID:$UID github-split
+# ask sudo accesss if not already available
+if [[ -z $(sudo -n uptime 2>/dev/null) ]]; then
+    log warn sudo access required
+    sudo echo >/dev/null
+    # one more check if the user abort the password question
+    [[ -z `sudo -n uptime 2>/dev/null` ]] && { err abort sudo required; exit 1; }
+fi
+
+log install github-split
+if [[ -n $(which curl) ]]
+then
+    sudo curl raw.githubusercontent.com/jeromedecoster/github-split/master/github-split \
+        --location \
+        --remote-name \
+        --progress-bar
+else
+    sudo wget raw.githubusercontent.com/jeromedecoster/github-split/master/github-split \
+        --quiet \
+        --no-clobber \
+        --show-progress
+fi
+
+sudo chmod +x github-split
 
 log complete github-split successfully installed
+exit 0
