4598b6e48416e02b3f26e83a294154480bc36f94
jeromedecoster <jeromedecoster@users.noreply.github.com>
2020-05-03 21:57:43 +0200

    :boom:

--- script.sh
+++ script.sh
@@ -3,6 +3,21 @@ set -e
 log() { echo -e "\e[38;5;82;4m${1}\e[0m \e[38;5;226m${@:2}\e[0m"; }
 err() { echo -e "\e[38;5;196;4m${1}\e[0m \e[38;5;87m${@:2}\e[0m" >&2; }
 
+download() {
+    if [[ -n $(which curl) ]]
+    then
+        sudo curl "$1" \
+            --location \
+            --remote-name \
+            --progress-bar
+    else
+        sudo wget "$1" \
+            --quiet \
+            --no-clobber \
+            --show-progress
+    fi
+}
+
 cd /usr/local/bin
 
 # abort if already installed
@@ -17,20 +32,14 @@ if [[ -z $(sudo -n uptime 2>/dev/null) ]]; then
 fi
 
 log install github-split
-if [[ -n $(which curl) ]]
-then
-    sudo curl raw.githubusercontent.com/jeromedecoster/github-split/master/github-split \
-        --location \
-        --remote-name \
-        --progress-bar
-else
-    sudo wget raw.githubusercontent.com/jeromedecoster/github-split/master/github-split \
-        --quiet \
-        --no-clobber \
-        --show-progress
-fi
+download raw.githubusercontent.com/jeromedecoster/github-split/master/github-split
 
 sudo chmod +x github-split
+
+mkdir --parents ~/.config/github-split
+cd ~/.config/github-split
+download raw.githubusercontent.com/jeromedecoster/github-split/master/README.md.tpl
+download raw.githubusercontent.com/jeromedecoster/github-split/master/script.sh.tpl
 
 log complete github-split successfully installed
 exit 0
